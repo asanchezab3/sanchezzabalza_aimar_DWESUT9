d06353ea08a57d529b9bcb68dfac2839a9b543b7
Aimar Sánchez Zabalza <asanchezab3@educacion.navarra.es>
2024-04-20 12:02:10 +0200

    Creada entidad, COntrollers y editado base.html

---
+++ src/Entity/Users.php
@@ -0,0 +1,103 @@
+<?php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Table(name="users")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(name="apellido1", type="string", length=100)
+     */
+    private $apellido1;
+
+    /**
+     * @ORM\Column(name="apellido2", type="string", length=100)
+     */
+    private $apellido2;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $correo;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $pass;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+    public function getApellido1(): ?string
+    {
+        return $this->apellido1;
+    }
+
+    public function setApellido1(string $apellido1): self
+    {
+        $this->apellido1 = $apellido1;
+        return $this;
+    }
+
+    public function getApellido2(): ?string
+    {
+        return $this->apellido2;
+    }
+
+    public function setApellido2(string $apellido2): self
+    {
+        $this->apellido2 = $apellido2;
+        return $this;
+    }
+
+    public function getCorreo(): ?string
+    {
+        return $this->correo;
+    }
+
+    public function setCorreo(string $correo): self
+    {
+        $this->correo = $correo;
+        return $this;
+    }
+
+    public function getPass(): ?string
+    {
+        return $this->pass;
+    }
+
+    public function setPass(string $pass): self
+    {
+        $this->pass = $pass;
+        return $this;
+    }
+}
